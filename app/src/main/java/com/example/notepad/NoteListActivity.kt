63a0016a8ca236cc09f7d5edbfd8bbf7ebc3a148
Ken <ken.su_dev@hotmail.com>
2021-02-25 20:58:39 +0100

    Add FavoritesNotes, AllNotes and DeletedNotes in one activity

--- app/src/main/java/com/example/notepad/NoteListActivity.kt
+++ app/src/main/java/com/example/notepad/NoteListActivity.kt
@@ -17,24 +17,36 @@ class NoteListActivity : AppCompatActivity(), View.OnClickListener {
 
     //region ========================================== Val or Var ==========================================
 
-    private lateinit var listOfNotes: MutableList<Note>
+    private lateinit var listOfAllNotes: MutableList<Note>
+    private lateinit var listOfFavoritesNotes: MutableList<Note>
+    private lateinit var listOfDeletedNotes: MutableList<Note>
+
+    private var fromAllNotes = true
+    private var fromFavorites = false
+    private var fromDeletedNotes = false
+
     private lateinit var adapter: RecyclerViewNoteAdapter
+    private lateinit var recyclerView: RecyclerView
 
     private var noteDao = App.database.noteDao()
+
+    private lateinit var menuItemListFavorites: MenuItem
 
     private val mOnNavigationItemSelectedListener =
         BottomNavigationView.OnNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.activity_note_list_bottom_nav_view_notes -> {
+                    toAllNotes()
                     return@OnNavigationItemSelectedListener true
                 }
                 R.id.activity_note_list_bottom_nav_view_favorites -> {
-                    startActivity(Intent(this, NoteListFavoritesActivity::class.java))
-                    overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
                     menuItem.setIcon(R.drawable.ic_full_star)
+                    toFavorites()
+
                     return@OnNavigationItemSelectedListener true
                 }
                 R.id.activity_note_list_bottom_nav_view_trash -> {
+                    toDeletedNotes()
                     return@OnNavigationItemSelectedListener true
                 }
                 R.id.activity_note_list_bottom_nav_view_categories -> {
@@ -50,8 +62,11 @@ class NoteListActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_list)
 
-        listOfNotes = noteDao.getAllNotes()
-        adapter = RecyclerViewNoteAdapter(listOfNotes, this)
+        listOfAllNotes = noteDao.getAllNotes()
+        listOfFavoritesNotes = noteDao.getFavoritesNotes()
+        listOfDeletedNotes = noteDao.getDeletedNotes()
+
+        adapter = RecyclerViewNoteAdapter(listOfAllNotes, this)
 
         val toolbar = findViewById<Toolbar>(R.id.activity_note_list_toolbar)
         setSupportActionBar(toolbar)
@@ -61,9 +76,8 @@ class NoteListActivity : AppCompatActivity(), View.OnClickListener {
             findViewById<FloatingActionButton>(R.id.activity_note_list_floating_button)
         floatingButton.setOnClickListener(this)
 
-        val recyclerView = findViewById<RecyclerView>(R.id.activity_note_list_recyclerview)
-        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        recyclerView.adapter = adapter
+        recyclerView = findViewById(R.id.activity_note_list_recyclerview)
+        recyclerViewInit(adapter)
 
         val bottomNavigationView =
             findViewById<BottomNavigationView>(R.id.activity_note_list_bottom_nav_view)
@@ -72,6 +86,7 @@ class NoteListActivity : AppCompatActivity(), View.OnClickListener {
         val navItem = menu.findItem(R.id.activity_note_list_bottom_nav_view_notes)
         navItem.isChecked = true
         bottomNavigationView!!.menu.getItem(0).isChecked = true
+        menuItemListFavorites = menu.findItem(R.id.activity_note_list_bottom_nav_view_favorites)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -93,7 +108,13 @@ class NoteListActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(v: View?) {
         if (v?.tag != null) {
-            showNoteDetail(v.tag as Int)
+            if (fromAllNotes) {
+                showNoteDetail(v.tag as Int, listOfAllNotes)
+            } else if (fromFavorites) {
+                showNoteDetail(v.tag as Int, listOfFavoritesNotes)
+            } else if (fromDeletedNotes) {
+                showNoteDetail(v.tag as Int, listOfDeletedNotes)
+            }
         } else {
             when (v?.id) {
                 R.id.activity_note_list_floating_button -> createNewNote()
@@ -101,8 +122,8 @@ class NoteListActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-    private fun showNoteDetail(noteIndex: Int) {
-        val note = listOfNotes[noteIndex]
+    private fun showNoteDetail(noteIndex: Int, notes: MutableList<Note>) {
+        val note = notes[noteIndex]
 
         val intent = Intent(this, NoteDetailActivity::class.java)
         intent.putExtra(NoteDetailActivity.EXTRA_NOTE_ID, note.id)
@@ -112,5 +133,41 @@ class NoteListActivity : AppCompatActivity(), View.OnClickListener {
     private fun createNewNote() {
         val intent = Intent(this, NoteDetailActivity::class.java)
         startActivity(intent)
+    }
+
+    private fun recyclerViewInit(recyclerViewNoteAdapter: RecyclerViewNoteAdapter) {
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        recyclerView.adapter = recyclerViewNoteAdapter
+        recyclerViewNoteAdapter.notifyDataSetChanged()
+    }
+
+    private fun toFavorites() {
+        adapter = RecyclerViewNoteAdapter(listOfFavoritesNotes, this)
+        recyclerViewInit(adapter)
+        menuItemListFavorites.setIcon(R.drawable.ic_full_star)
+
+        fromAllNotes = false
+        fromFavorites = true
+        fromDeletedNotes = false
+    }
+
+    private fun toAllNotes() {
+        adapter = RecyclerViewNoteAdapter(listOfAllNotes, this)
+        recyclerViewInit(adapter)
+        menuItemListFavorites.setIcon(R.drawable.ic_star)
+
+        fromAllNotes = true
+        fromFavorites = false
+        fromDeletedNotes = false
+    }
+
+    private fun toDeletedNotes() {
+        adapter = RecyclerViewNoteAdapter(listOfDeletedNotes, this)
+        recyclerViewInit(adapter)
+        menuItemListFavorites.setIcon(R.drawable.ic_star)
+
+        fromAllNotes = false
+        fromFavorites = false
+        fromDeletedNotes = true
     }
 }
