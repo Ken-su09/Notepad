45a30650a8daa22b5b0c57f6497f3b76c17711d1
Ken <ken.su_dev@hotmail.com>
2021-03-03 12:41:09 +0100

    Add SharedPref to manage the good BottomNavView Item

--- app/src/main/java/com/example/notepad/NoteListActivity.kt
+++ app/src/main/java/com/example/notepad/NoteListActivity.kt
@@ -1,7 +1,8 @@
 package com.example.notepad
 
-import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
@@ -28,6 +29,11 @@ class NoteListActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var adapter: RecyclerViewNoteAdapter
     private lateinit var recyclerView: RecyclerView
 
+    private var bottomNavigationView: BottomNavigationView? = null
+    private var floatingButton: FloatingActionButton? = null
+
+    private var sharedPref: SharedPreferences? = null
+
     private var noteDao = App.database.noteDao()
 
     private lateinit var menuItemListFavorites: MenuItem
@@ -36,17 +42,34 @@ class NoteListActivity : AppCompatActivity(), View.OnClickListener {
         BottomNavigationView.OnNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.activity_note_list_bottom_nav_view_notes -> {
+                    val editor: SharedPreferences.Editor = sharedPref!!.edit()
+                    editor.putInt("bottomNavigationView", 0)
+                    editor.apply()
+                    editor.commit()
+
                     toAllNotes()
+
                     return@OnNavigationItemSelectedListener true
                 }
                 R.id.activity_note_list_bottom_nav_view_favorites -> {
+                    val editor: SharedPreferences.Editor = sharedPref!!.edit()
+                    editor.putInt("bottomNavigationView", 1)
+                    editor.apply()
+                    editor.commit()
                     menuItem.setIcon(R.drawable.ic_full_star)
+
                     toFavorites()
 
                     return@OnNavigationItemSelectedListener true
                 }
                 R.id.activity_note_list_bottom_nav_view_trash -> {
+                    val editor: SharedPreferences.Editor = sharedPref!!.edit()
+                    editor.putInt("bottomNavigationView", 2)
+                    editor.apply()
+                    editor.commit()
+
                     toDeletedNotes()
+
                     return@OnNavigationItemSelectedListener true
                 }
                 R.id.activity_note_list_bottom_nav_view_categories -> {
@@ -62,32 +85,57 @@ class NoteListActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_list)
 
+        //region ======================================== getNoteDAO ========================================
+
         listOfAllNotes = noteDao.getAllNotes()
         listOfFavoritesNotes = noteDao.getFavoritesNotes()
         listOfDeletedNotes = noteDao.getDeletedNotes()
 
-        adapter = RecyclerViewNoteAdapter(listOfAllNotes, this)
+        //endregion
+
+        sharedPref = getSharedPreferences("bottomNavigationView", Context.MODE_PRIVATE)
+        val bottomNavPosition = sharedPref!!.getInt("bottomNavigationView", 0)
+
+        //region ======================================== Toolbar ========================================
 
         val toolbar = findViewById<Toolbar>(R.id.activity_note_list_toolbar)
         setSupportActionBar(toolbar)
         supportActionBar!!.setDisplayShowTitleEnabled(false)
 
-        val floatingButton =
-            findViewById<FloatingActionButton>(R.id.activity_note_list_floating_button)
-        floatingButton.setOnClickListener(this)
+        //endregion
+
+        //region ======================================= FindViewById =======================================
 
         recyclerView = findViewById(R.id.activity_note_list_recyclerview)
+        floatingButton = findViewById(R.id.activity_note_list_floating_button)
+        bottomNavigationView = findViewById(R.id.activity_note_list_bottom_nav_view)
+
+        //endregion
+
+        adapter = RecyclerViewNoteAdapter(listOfAllNotes, this)
         recyclerViewInit(adapter)
 
-        val bottomNavigationView =
-            findViewById<BottomNavigationView>(R.id.activity_note_list_bottom_nav_view)
-        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
-        val menu = bottomNavigationView.menu
+        val fromRestoreNote = intent.getIntExtra("restoreNote", 1)
+        if (fromRestoreNote == 0) {
+            bottomNavigationView!!.menu.getItem(fromRestoreNote).isChecked = true
+        } else {
+            bottomNavigationView!!.menu.getItem(bottomNavPosition).isChecked = true
+        }
+
+        val menu = bottomNavigationView!!.menu
         val navItem = menu.findItem(R.id.activity_note_list_bottom_nav_view_notes)
         navItem.isChecked = true
-        bottomNavigationView!!.menu.getItem(0).isChecked = true
         menuItemListFavorites = menu.findItem(R.id.activity_note_list_bottom_nav_view_favorites)
+
+        //region ========================================= Listeners ========================================
+
+        bottomNavigationView!!.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        floatingButton!!.setOnClickListener(this)
+
+        //endregion
     }
+
+    //region =========================================== Listeners ==========================================
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.note_list_toolbar_menu, menu)
@@ -108,12 +156,16 @@ class NoteListActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(v: View?) {
         if (v?.tag != null) {
-            if (fromAllNotes) {
-                showNoteDetail(v.tag as Int, listOfAllNotes)
-            } else if (fromFavorites) {
-                showNoteDetail(v.tag as Int, listOfFavoritesNotes)
-            } else if (fromDeletedNotes) {
-                showNoteDetail(v.tag as Int, listOfDeletedNotes)
+            when {
+                fromAllNotes -> {
+                    showNoteDetail(v.tag as Int, listOfAllNotes)
+                }
+                fromFavorites -> {
+                    showNoteDetail(v.tag as Int, listOfFavoritesNotes)
+                }
+                fromDeletedNotes -> {
+                    showNoteDetail(v.tag as Int, listOfDeletedNotes)
+                }
             }
         } else {
             when (v?.id) {
@@ -121,6 +173,10 @@ class NoteListActivity : AppCompatActivity(), View.OnClickListener {
             }
         }
     }
+
+    //endregion
+
+    //region =========================================== Functions ==========================================
 
     private fun showNoteDetail(noteIndex: Int, notes: MutableList<Note>) {
         val note = notes[noteIndex]
@@ -170,4 +226,6 @@ class NoteListActivity : AppCompatActivity(), View.OnClickListener {
         fromFavorites = false
         fromDeletedNotes = true
     }
+
+    //endregion
 }
